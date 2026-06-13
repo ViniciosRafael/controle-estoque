b755ba4b1199e7ea77d2d574b39fbe1f2984fabd
Vinicios Rafael Schimuneck <viniciosrafaelschimuneck@gmail.com>
2026-06-13 19:31:32 -0300

    Primeiro commit e inicio do projeto

---
+++ Contole_estoque/src/main/java/com/mycompany/contole_estoque/Contole_estoque.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contole_estoque;
+
+/**
+ *
+ * @author vinic
+ */
+public class Contole_estoque {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
